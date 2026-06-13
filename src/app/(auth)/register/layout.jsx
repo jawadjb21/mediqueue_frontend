740c3f88b415b59940fad3e835a55cf540258dd7
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-13 07:32:14 +0200

    feat(seo): auth page layout and metadata

---
+++ src/app/(auth)/register/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Register',
+    description: 'Create Your MediQueue Account',
+}
+
+const RegisterLayout = ({ children }) => {
+    return children;
+};
+
+export default RegisterLayout;
