ae4c906d1eab22357de3bad79912b034d7c37dc8
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-15 22:32:41 +0200

    build: add tutor

--- src/lib/postTutor.js
+++ src/lib/postTutor.js
@@ -1,3 +1,29 @@
-export const postTutor = async (data) => {
-    console.log(data);
+"use server";
+
+import { refresh, revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const postTutor = async (formData) => {
+    try {
+        const request = await fetch(`${process.env.SERVER_URL}/add-tutors`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(formData),
+        });
+        if(!request.ok){
+            throw new Error("Server request failed!")
+        }
+        const response = await request.json();
+        if(!response.acknowledged){
+            throw new Error("Database insertion failed!")
+        }
+    } catch (error){
+        console.error(error);
+        return null;
+    } finally{
+        revalidatePath("/tutors");
+        redirect("/");
+    }
 }
