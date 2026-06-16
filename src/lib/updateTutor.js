f89d37aae52f6882d105c94f73780e8adc4deac7
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-06-16 21:01:17 +0200

    feat: update tutor info

---
+++ src/lib/updateTutor.js
@@ -0,0 +1,31 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const updateTutor = async (formData) => {
+    if (!formData) {
+        return "No tutor found!"
+    }
+    try {
+        const request = await fetch(`${process.env.SERVER_URL}/tutors/${formData?.tutorId}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(formData),
+            
+        });
+        if(!request.ok){
+            throw new Error("Server request failed!");
+        }
+        const response = await request.json();
+        console.log(response);
+    } catch(error){
+        console.error(error);
+        return "Couldn't update tutor."
+    } finally{
+        revalidatePath("/tutors");
+        revalidatePath("/my-tutors");
+        redirect("/my-tutors");
+    }
+}
